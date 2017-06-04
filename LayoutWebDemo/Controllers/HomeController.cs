f3a924720004c12a83200704c7e303529b38db90
侯攀 <houpan135@outlook.com>
2017-06-04 13:42:52 +0800

    添加项目文件。

---
+++ LayoutWebDemo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutWebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Default()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return PartialView();
+        }
+
+        [ActionName("OurContact")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return PartialView("Contact");
+        }
+
+        [ChildActionOnly]
+        public ActionResult SayHi()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ LayoutWebDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LayoutWebDemo.MvcApplication" Language="C#" %>
